894594c12d9bb2e9da03c9238fdf9b7239a097d2
julietamattei <166766751+julietamattei@users.noreply.github.com>
2024-05-14 21:08:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var nombre="Julieta"; //string
+var apellido="Mattei"; //string
+
+console.log(nombre + " " + apellido)
+
+var numero1=20; //number
+var numero2=5; //number
+
+var suma=numero1+numero2
+
+console.log(suma=numero1+numero2)
+
+var suma=suma
+
+console.log(suma+numero2)
